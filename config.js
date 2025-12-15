138fa3ba2d265688f15284a6bffeda6f12ae8c9c
alexandryasukaitisss-cmd <alexandryasukaitisss@gmail.com>
2025-12-15 20:55:03 +0300

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   // Вставьте сюда Client ID из Yandex OAuth
-  CLIENT_ID: "PASTE_YOUR_CLIENT_ID",
+  CLIENT_ID: "a2875e402bc048d3b804a12d1ccdb272",
 
   // ВАЖНО: должен совпадать с Redirect URI в настройках приложения
   REDIRECT_URI: window.location.origin + window.location.pathname.replace(/\/[^/]*$/, "/callback.html"),
