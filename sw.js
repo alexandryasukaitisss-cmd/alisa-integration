61f3a56727611a6b042d9d692a630144c7d92a23
alexandryasukaitisss-cmd <alexandryasukaitisss@gmail.com>
2025-12-15 21:29:14 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "ya-home-pwa-v1";
+const CACHE = "ya-home-pwa-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -14,8 +14,13 @@ self.addEventListener("install", (e) => {
 });
 
 self.addEventListener("activate", (e) => {
-  e.waitUntil(self.clients.claim());
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
+
 
 self.addEventListener("fetch", (e) => {
   const url = new URL(e.request.url);
